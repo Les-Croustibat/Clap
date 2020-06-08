cc3f2d774f9b693997e6cadba2d6ee7c1bc14e2c
fabien <fabien.pergaud@yahoo.fr>
2020-06-08 16:35:15 +0200

    route

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -8,15 +8,15 @@ class ErrorController extends AbstractController
 {
     public function error403()
     {
-        return $this->render('error/error403.html.twig');
+        return $this->render('error/error_403.html.twig');
     }
     public function error404()
     {
-        return $this->render('error/error404.html.twig');
+        return $this->render('error/error_404.html.twig');
     }
     public function error500()
     {
-        return $this->render('error/error500.html.twig');
+        return $this->render('error/error_500.html.twig');
     }
 
 }
