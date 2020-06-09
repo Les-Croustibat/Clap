5d2165fd49b0168cd2f41d73eb2d9ae033700d66
ChristopheBorios <boriosc@gmail.com>
2020-06-09 17:55:24 +0200

    messages erreurs formulaire contact

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,14 +15,13 @@ class DefaultController extends AbstractController
 
     public function contact(MailerInterface $mailer)
     {
-
-        if(isset($_POST) && !empty($_POST)){
-                
+        $errors = [];
+        if(isset($_POST) && !empty($_POST)){  
             if($this->verifForm($_POST, ['lastname', 'firstname', 'email', 'object', 'message'])){
 
                     $safe = array_map('strip_tags', $_POST);
 
-                    $error = [
+                    $errors = [
                         (empty($safe['lastname'])) ? 'Votre nom doit être valide' : null,
                         (empty($safe['firstname'])) ? 'Votre prénom doit être valide' : null,
                         (!filter_var($safe['email'], FILTER_VALIDATE_EMAIL)) ? 'Votre mail doit être valide' : null,
@@ -32,29 +31,30 @@ class DefaultController extends AbstractController
 
                     $errors = array_filter($errors);
         
-                    $messageHTML = '<p>Bonjour,';
-                    $messageHTML.= '<br>Vous avez été contacté par le formulaire de votre site.</p>';
-                    $messageHTML.= '<ul>';
-                    $messageHTML.= '<li>Nom & prénom : '.mb_strtoupper($safe['lastname']).' '.$safe['firstname'].'</li>';
-                    $messageHTML.= '<li>Objet : '.$safe['object'].' '.'</li>';
-                    $messageHTML.= '<li>Message : '.nl2br($safe['message']).' '.'</li>';
-                    $messageHTML.= '</ul>';
+                    if(count($errors) == 0){
+                        $messageHTML = '<p>Bonjour,';
+                        $messageHTML.= '<br>Vous avez été contacté par le formulaire de votre site.</p>';
+                        $messageHTML.= '<ul>';
+                        $messageHTML.= '<li>Nom & prénom : '.mb_strtoupper($safe['lastname']).' '.$safe['firstname'].'</li>';
+                        $messageHTML.= '<li>Objet : '.$safe['object'].' '.'</li>';
+                        $messageHTML.= '<li>Message : '.nl2br($safe['message']).' '.'</li>';
+                        $messageHTML.= '</ul>';
 
-                    $email = new Email();
-                    $email->from($safe['email']);
-                    $email->to('contact@clap.fr');
-                    $email->replyTo($safe['email']);
-    
-                    $email->subject('Contact du site en date du '.date('d/m/Y H:i'));
-                    $email->html($messageHTML); 
-                    $email->text(strip_tags($messageHTML)); 
+                        $email = new Email();
+                        $email->from($safe['email']);
+                        $email->to('contact@clap.fr');
+                        $email->replyTo($safe['email']);
+        
+                        $email->subject('Contact du site en date du '.date('d/m/Y H:i'));
+                        $email->html($messageHTML); 
+                        $email->text(strip_tags($messageHTML)); 
 
-                    $mailer->send($email);
-                    $this->addFlash('success', 'Votre message a bien été envoyé');
-    
-            } else {
-                $errorsMessage = implode('<br>', $errors);
-                $this->addFlash('danger', $errorsMessage);
+                        $mailer->send($email);
+                        $this->addFlash('success', 'Votre message a bien été envoyé');
+                    } else {
+                        $errorsMessage = implode('<br>', $errors);
+                        $this->addFlash('danger', $errorsMessage);
+                    }
             }
     
         }
@@ -63,7 +63,6 @@ class DefaultController extends AbstractController
             'input' => $safe ?? [], 
         ]);
     }
-    
     
     
     private function verifForm($superglobale, $champs)
