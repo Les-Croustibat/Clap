daf3fc4fbb72ec2c7476094825da789adbf317f8
ChristopheBorios <boriosc@gmail.com>
2020-06-09 16:21:27 +0200

    Flèche de retour

--- public/asset/script/script.js
+++ public/asset/script/script.js
@@ -1,3 +1,20 @@
-$( "#arrow" ).click(function() {
-    alert( "Handler for .click() called." );
-  });
+$(document).ready(function(){
+   
+    let btn = $('#arrow');
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 300){
+            btn.addClass('show');
+        }else{
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('body').animate({
+            scrollTop: 0
+        }, 500);
+      });
+
+});
