f582d5b721279d8e19a11696c9ddc65df07ef406
katethegreat-coder <aureliedutrey@hotmail.com>
2020-06-09 15:30:06 +0200

    Test Call APi

--- src/Controller/APIAllocineController.php
+++ src/Controller/APIAllocineController.php
@@ -8,13 +8,183 @@ use Symfony\Component\Routing\Annotation\Route;
 class APIAllocineController extends AbstractController
 {
     
-    public function callAPI()
+    public function callAPIPerson($search=null)
     {
+        if(empty ($search)) {
 
+            // if API not called with parameters, no search
+
+            return false;
+
+        }  else {
+
+            // Search parameters => a person (actor, director) then call the API
+            // Define the URL
+            $api_url="http://api.allocine.fr/rest/v3/search";
+            $timeout = 10; 
+            // String to search
+            $q='?'.http_build_query($search).'partner=QUNXZWItQWxsb0Npbuk&filter=person&format=json';
+
+            // Initialize the curl
+            $curl = curl_init();
+
+            // Set the curl options
+            $options=[
+                CURLOPT_URL            => $api_url, // target the API URL
+                CURLOPT_RETURNTRANSFER => true, // return the content into a string
+                CURLOPT_CONNECTTIMEOUT => $timeout, // set a timeout i.e. maximum time the connection is allowed to take 
+                CURLOPT_TIMEOUT        => $timeout, // set a timeout i.e. maximum time the request is allowed to take 
+                CURLOPT_USERAGENT      => self::getRandomUserAgent(),
+            
+            ];
+            
+            // Error message
+            if(empty($curl)){
+                die("ERREUR curl_init : cURL is not available.");
+            }
+
+            // Config download options
+            curl_setopt_array($curl,$options);
+        
+            // Execute the query
+            $response=curl_exec($curl);            
+        
+            // Close
+            curl_close($curl);
+
+            // Decode the response (true, key and value -> PHP)
+            $decode_response=json_decode($response, true);
+
+
+        }
+        
         // DO NOT FORGET !!! no render just a return of data
-        return $this->render('api_allocine/index.html.twig', [
-            'controller_name' => 'APIAllocineController',
-        ]);
+        return $decode_response;
     }
     
+    public function callAPIMovie($movie=null)
+    {
+        if(empty ($movie)) {
+
+            // if API not called with parameters, no search
+
+            return false;
+
+        }  else {
+
+            // Search parameters => a person (actor, director) then call the API
+            // Define the URL
+            $api_url="http://api.allocine.fr/rest/v3/search";
+            $timeout = 10; 
+            // String to search
+            $q='?'.http_build_query($movie).'partner=QUNXZWItQWxsb0Npbuk&filter=movie&format=json';
+
+            // Initialize the curl
+            $curl = curl_init();
+
+            // Set the curl options
+            $options=[
+                CURLOPT_URL            => $api_url, // target the API URL
+                CURLOPT_RETURNTRANSFER => true, // return the content into a string
+                CURLOPT_CONNECTTIMEOUT => $timeout, // set a timeout i.e. maximum time the connection is allowed to take 
+                CURLOPT_TIMEOUT        => $timeout, // set a timeout i.e. maximum time the request is allowed to take 
+                CURLOPT_USERAGENT      => self::getRandomUserAgent(),
+            
+            ];
+            
+            // Error message
+            if(empty($curl)){
+                die("ERREUR curl_init : cURL is not available.");
+            }
+
+            // Config download options
+            curl_setopt_array($curl,$options);
+        
+            // Execute the query
+            $response=curl_exec($curl);            
+        
+            // Close
+            curl_close($curl);
+
+            // Decode the response (true, key and value -> PHP)
+            $decode_response=json_decode($response, true);
+
+            // Search parameters => a person (actor, director) then call the API
+            // Define the URL
+            $api_url="http://api.allocine.fr/rest/v3/movie";
+            $timeout = 10; 
+            // String to search
+            $q='?'.http_build_query($movie).'partner=QUNXZWItQWxsb0Npbuk&filter=movie&code=format=json';
+
+            // Initialize the curl
+            $curl = curl_init();
+
+            // Set the curl options
+            $options=[
+                CURLOPT_URL            => $api_url, // target the API URL
+                CURLOPT_RETURNTRANSFER => true, // return the content into a string
+                CURLOPT_CONNECTTIMEOUT => $timeout, // set a timeout i.e. maximum time the connection is allowed to take 
+                CURLOPT_TIMEOUT        => $timeout, // set a timeout i.e. maximum time the request is allowed to take
+            
+            ];
+            
+            // Error message
+            if(empty($curl)){
+                die("ERREUR curl_init : cURL is not available.");
+            }
+
+            // Config download options
+            curl_setopt_array($curl,$options);
+        
+            // Execute the query
+            $response=curl_exec($curl);            
+        
+            // Close
+            curl_close($curl);
+
+            // Decode the response (true, key and value -> PHP)
+            $decode_response=json_decode($response, true);
+
+            return $decode_response;
+
+        }
+    }
+
+
+    private function getRandomUserAgent()
+	{
+		$v = rand(1, 4).'.'.rand(0, 9);
+		$a = rand(0, 9);
+		$b = rand(0, 99);
+		$c = rand(0, 999);
+
+
+		$userAgents = [
+					"Mozilla/5.0 (Linux; U; Android $v; fr-fr; Nexus One Build/FRF91) AppleWebKit/5$b.$c (KHTML, like Gecko) Version/$a.$a Mobile Safari/5$b.$c",
+					"Mozilla/5.0 (Linux; U; Android $v; fr-fr; Dell Streak Build/Donut AppleWebKit/5$b.$c+ (KHTML, like Gecko) Version/3.$a.2 Mobile Safari/ 5$b.$c.1",
+					"Mozilla/5.0 (Linux; U; Android 4.$v; fr-fr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+					"Mozilla/5.0 (Linux; U; Android 4.$v; fr-fr; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+					"Mozilla/5.0 (Linux; U; Android $v; en-gb) AppleWebKit/999+ (KHTML, like Gecko) Safari/9$b.$a",
+					"Mozilla/5.0 (Linux; U; Android $v.5; fr-fr; HTC_IncredibleS_S710e Build/GRJ$b) AppleWebKit/5$b.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5$b.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; HTC Vision Build/GRI$b) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android $v.4; fr-fr; HTC Desire Build/GRJ$b) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; T-Mobile myTouch 3G Slide Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android $v.3; fr-fr; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; HTC Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5$b.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; LG-LU3000 Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5$b.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; HTC_DesireS_S510e Build/GRI$a) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/$c.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile",
+					"Mozilla/5.0 (Linux; U; Android $v.3; fr-fr; HTC Desire Build/GRI$a) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android 2.$v; fr-fr; HTC Desire Build/FRF$a) AppleWebKit/533.1 (KHTML, like Gecko) Version/$a.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android $v; fr-lu; HTC Legend Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/$a.$a Mobile Safari/$c.$a",
+					"Mozilla/5.0 (Linux; U; Android $v; fr-fr; HTC_DesireHD_A9191 Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+					"Mozilla/5.0 (Linux; U; Android $v.1; fr-fr; HTC_DesireZ_A7$c Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/$c.$a",
+					"Mozilla/5.0 (Linux; U; Android $v.1; en-gb; HTC_DesireZ_A7272 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/$c.1",
+					"Mozilla/5.0 (Linux; U; Android $v; fr-fr; LG-P5$b Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+				];
+
+
+		return $userAgents[rand(0, count($userAgents) - 1)];
+	}
 }
