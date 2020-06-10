30e1cf5e4a786a001a4ee2d6717fd7d73b6ff81c
Chris <boriosc@gmail.COM>
2020-06-11 00:00:04 +0200

    Réflexion algorithme au hassard

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -16,18 +16,25 @@ class MovieController extends AbstractController
         $movie_details = $apiAllocine->callAPIPartner($random);
         dump($movie_details);
 
-        $movie_title = $movie_details['movie']['originalTitle'];
-        $movie_link = $movie_details['movie']['link']['0']['href'];
+        //$movie_title = $movie_details['movie']['originalTitle'];
+        //$movie_link = $movie_details['movie']['link']['0']['href'];
         
+        if(!empty($_POST)){
+            if(isset($_POST['action']) && $_POST['action'] === 'hasard') {
+                $movie_title = $movie_details['movie']['originalTitle'];
+            }
+        }
+
         return $this->render('movie/movie_find.html.twig', [
             'titre' => $movie_title ?? [],
-            'link' => $movie_link ?? [],
+            // 'link' => $movie_link ?? [],
         ]);
     }
 
     // No result: fonction ou solution pour emêcher ça (voir taille du max de rand)
     // Ajax
     // Créer une fonction qui trouve le nombre le plus élevé
-    //Si error i-- return i
+    //Si error i-- return i*
+    //Faire apparaître la balise div quand on appuie sur le bouton display hidden et block
 
 }
