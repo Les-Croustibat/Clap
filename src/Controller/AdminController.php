54b1064c313983971c3bd0d0956d03a74493eb3b
Cyrielle <63300474+cyrielletouton@users.noreply.github.com>
2020-06-12 10:21:41 +0200

    admin list view suppr

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User; // J'appelle l'entity dans laquelle je vais enregistrer mes données => correspond à une table SQL
+
+class AdminController extends AbstractController
+{
+    public function list()
+    {
+        // On accède au manager (connexion DB)
+        $entityManager = $this->getDoctrine()->getManager();
+        // On précise dans quelle table on veut agir via le repository et on execute la fonction
+        $users_in_db = $entityManager->getRepository(User::class)->findAll();
+
+        return $this->render('admin/users_list.html.twig', [
+            'users' => $users_in_db,
+        ]);
+    }
+
+    public function view(int $id)
+    {
+        // On accède au manager (connexion DB)
+        $entityManager = $this->getDoctrine()->getManager();
+        // On précise dans quelle table on veut agir via le repository et on execute la fonction
+        $users_in_db = $entityManager->getRepository(User::class)->find($id);
+
+        return $this->render('admin/users_view.html.twig', [
+            'user' => $users_in_db,
+        ]);
+    }
+
+    public function suppr(int $id)
+    {
+        // On accède au manager (connexion DB)
+        $entityManager = $this->getDoctrine()->getManager();
+        // On précise dans quelle table on veut agir via le repository et on execute la fonction
+        $user_in_db = $entityManager->getRepository(User::class)->find($id);
+
+        if (!empty($_POST)) {
+            if (isset($_POST['action']) && $_POST['action'] === 'delete') {
+
+                // Suppression
+                $entityManager->remove($user_in_db);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'L\'utilisateur a bien été supprimé');
+                return $this->redirectToRoute('admin_list');
+            }
+        }
+
+        return $this->render('admin/users_suppr.html.twig', [
+            'user' => $user_in_db,
+        ]);
+    }
+}
