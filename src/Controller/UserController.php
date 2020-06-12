92d564830e3015cc5f046f2c5fa76a7d0bdaa132
fabien <fabien.pergaud@yahoo.fr>
2020-06-12 16:00:25 +0200

    last

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,22 +9,23 @@ class UserController extends AbstractController
 {
     public function userSpace()
     {
-        // coucou
-        $Api= new APIAllocineController;
-        $resultat = $Api -> callAPIPartner('will smith','person');
-        dd($resultat);
-
-        return $this->render('user/user_space.html.twig');
+       
+        // $Api= new APIAllocineController;
+        // $resultat = $Api -> callAPIPartner('will smith','person');
+        // return $this->render('user/user_space.html.twig');
     }
 
 
     // créer une route spécifique qui permettra d'accéder a cette page, meme si elle sera invisible
     public function ajaxApiperson(){
+        // $errors=[];
         if(!empty($_GET)){
-            dd($_GET);
             // nettoie les donnees
             $safe = array_map('trim', array_map('strip_tags', $_GET));
 
+            // if (strlen($safe['acteur']) < 3 || strlen($safe['acteur']) > 20) {
+                // $errors[] = 'Nom doit être compris entre 3 et 20 caractères';
+        // }
             $Api= new APIAllocineController();
             $resultat = $Api -> callAPIPartner($safe['actor'],'person');
 
@@ -33,7 +34,7 @@ class UserController extends AbstractController
                 return $this->json(['status' => 'ko', 'error' => 'Désolé, cette personne est introuvable']);
 
             }else{
-                $retourJSON= $resultat['feed']['person'][0]['realName'];
+                $retourJSON= $resultat['feed']['person'][0]['name'];
                 return $this->json(['status' => 'ok', 'result' =>$retourJSON]);
             }
 
@@ -42,5 +43,10 @@ class UserController extends AbstractController
             return $this->json(['error'=>'Aucune donnée reçues']);
         }
     }
+
+    // enregistrement des infos de mon-espace dans la bdd
+//     public function BddUserspace{
+
+//     }
 }
 
