5ee8edee0160b4210ad6ca645e3a660f3c1a54c7
Chris <boriosc@gmail.COM>
2020-06-13 19:48:22 +0200

    Virement du namespace APIAllocine

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Controller\APIAllocineController;
 
 class MovieController extends AbstractController
 {
